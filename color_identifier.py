4dabfad255b4700f6f8073c593e9db2fad4a5edc
Jorge Gonzalez-Heredia <heredia.jlg@Jorges-Laptop.local>
2026-01-12 17:11:57 -0600

    it worked for the 3 colors this time

--- color_identifier.py
+++ color_identifier.py
@@ -5,7 +5,7 @@ import visualkeras
 import numpy as np
 from image_utils import create_image, visualize_image, red_rbg, green_rbg, blue_rbg
 
-data_size = 100
+data_size = 1000
 
 green_images = [create_image(4, 'green') for _ in range(data_size)]
 red_images = [create_image(4, 'red') for _ in range(data_size)]
@@ -25,18 +25,21 @@ model = models.Sequential([
 ])
 
 model.compile(
-    optimizer=tf.keras.optimizers.Adam(0.001),
+    optimizer=tf.keras.optimizers.Adam(0.00001),
     loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
     metrics=['accuracy']
 )
 
 #red and green images
-x = np.array(red_images + green_images + blue_images, dtype=np.uint8)
-y = np.array([red_rbg for _ in range(data_size)] +
-             [green_rbg for _ in range(data_size)] +
-             [blue_rbg for _ in range(data_size)])
+x = np.array(red_images +
+             green_images +
+             blue_images,
+             dtype=np.uint8)
+y = np.array( [[1, 0, 0] for _ in range(data_size)] +
+              [[0, 1, 0] for _ in range(data_size)] +
+              [[0, 0, 1] for _ in range(data_size)])
 
-model.fit(x, y, epochs=100)
+model.fit(x, y, epochs=1000)
 
 red_image = create_image(4, 'red')
 green_image = create_image(4, 'green')
